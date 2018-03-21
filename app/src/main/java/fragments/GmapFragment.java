78239102bb18af87661b3d88677ba6dd7a1cb216
Monroe Stone <monroesawesome@gmail.com>
2018-03-21 00:17:16 -0400

    Big one, will add details to main merge

---
+++ app/src/main/java/fragments/GmapFragment.java
@@ -0,0 +1,44 @@
+package fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cpsc41400.a4140app.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by monro on 3/20/2018.
+ */
+
+public class GmapFragment extends Fragment implements OnMapReadyCallback {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gmap, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng Clemson = new LatLng(34.6834, -82.8374);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Clemson, 15));
+        
+    }
+}
