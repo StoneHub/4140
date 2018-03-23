d841ba854bf3c7357fdb6aae595f4c13f55caff6
root <root@young5-LP.localdomain>
2018-03-23 15:24:41 -0400

    randommarkers

--- app/src/main/java/fragments/GmapFragment.java
+++ app/src/main/java/fragments/GmapFragment.java
@@ -29,9 +29,12 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.Random;
 
 /**
  * Created by monro on 3/20/2018.
@@ -136,26 +139,21 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
             public void onClick(View view) {
                 Snackbar.make(view, "Current Coordinates: " + mLastKnownLocation.getLatitude()+ " " + mLastKnownLocation.getLongitude(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                LatLng[] messageCoord = new LatLng[10];
+                Random rand = new Random();
+                for(int x=0;x<10;x++){
+                    int lat = rand.nextInt(35)+33;
+                    int lon = rand.nextInt(81)+83;
+                    messageCoord[x] = new LatLng(lat,-lon);
+                }
+
+               // for(int x=0; x< 10; x++) {
+                    mMap.addMarker(new MarkerOptions().position(messageCoord[0]).title("Sender of Msg"));
+               // }
             }
         });
-
     }
-//    private void moveCamera(LatLng markerLoc) {
-////        LatLng markerLoc=new LatLng(mLastKnownLocation.getLatitude(),mLastKnownLocation.getLongitude());
-//        final CameraPosition cameraPosition = new CameraPosition.Builder()
-//                .target(markerLoc)      // Sets the center of the map to Mountain View
-//                .zoom(13)                   // Sets the zoom
-//                .build();                   //
-//        mMap.addMarker(new MarkerOptions().position(new LatLng(mDefaultLocation.longitude, mDefaultLocation.latitude)).title("Marker"));
-//        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-//        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
-////            @Override
-////            public boolean onMyLocationButtonClick() {
-////                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-////                return true;
-////            }
-////        });
-//    }
+
     /**
      * Gets the current location of the device, and positions the map's camera.
      */
