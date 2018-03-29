c45f6968f3e355aba14977ef95eefadc3b8b4811
StoneZone <monroesawesome@gmail.com>
2018-03-29 08:02:35 -0400

    Working on compose still

--- app/src/main/java/fragments/ComposeMsgFragment.java
+++ app/src/main/java/fragments/ComposeMsgFragment.java
@@ -1,13 +1,16 @@
 package fragments;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.annotation.XmlRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
 
 import com.cpsc41400.a4140app.R;
 
@@ -17,6 +20,7 @@ import com.cpsc41400.a4140app.R;
  */
 
 public class ComposeMsgFragment extends Fragment {
+
     @Nullable
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_compose, container, false);
@@ -24,12 +28,29 @@ public class ComposeMsgFragment extends Fragment {
         return rootView;
     }
 
-    public void noteLocationChange() {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        AutoCompleteTextView actv = getView().findViewById(R.id.whoNo);
 
+
+        int CONTACTS = 0;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                                        (getActivity(),android.R.layout.simple_list_item_1,CONTACTS);
+        actv.setAdapter(adapter);
+        actv.setAdapter(adapter);
+        actv.setOnItemClickListener(onItemClickListener);
     }
 
-    public void getContacts() {
-
-    }
+    private AdapterView.OnItemClickListener onItemClickListener =
+            new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Toast.makeText(getActivity(),
+                            "Clicked item from auto completion list "
+                                    + adapterView.getItemAtPosition(i)
+                            , Toast.LENGTH_SHORT).show();
+                }
+            };
 
 }
