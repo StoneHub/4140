5464da1c14c0bb9e3b50d799c6062a41238f7295
StoneZone <monroesawesome@gmail.com>
2018-03-29 15:50:34 -0400

    Added sweet little maps fragment to show confirmation and final look of message

--- app/src/main/java/fragments/ComposeMsgFragment.java
+++ app/src/main/java/fragments/ComposeMsgFragment.java
@@ -12,7 +12,14 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.cpsc41400.a4140app.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -25,6 +32,9 @@ public class ComposeMsgFragment extends Fragment {
     private static final String argKey = "argKey";
     private Button updateLocBtn;
 
+    MapView mMapView;
+    private GoogleMap googleMap;
+
     @Nullable
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_compose, container, false);
@@ -32,6 +42,8 @@ public class ComposeMsgFragment extends Fragment {
         double[] loc = getArguments().getDoubleArray(argKey);
         final String lat = Double.toString(loc[1]);
         final String lng = Double.toString(loc[0]);
+        final LatLng latLng = new LatLng(Double.parseDouble(lat),Double.parseDouble(lng));
+
 
 
         updateLocBtn = rootView.findViewById(R.id.changeLocBtn);
@@ -45,6 +57,27 @@ public class ComposeMsgFragment extends Fragment {
             }
         });
 
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+                // For dropping a marker at a point on the Map
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("Marker Title").snippet("Marker Description"));
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(15).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),3000,null);
+            }
+        });
         return rootView;
     }
 
