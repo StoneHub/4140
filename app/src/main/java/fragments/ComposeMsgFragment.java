e1573575f48dbdb3283105510de60ea333a68e00
Monroe Stone <monroesawesome@gmail.com>
2018-03-29 09:13:33 -0400

    Updated fragment transitions to use replaceFragment()

--- app/src/main/java/fragments/ComposeMsgFragment.java
+++ app/src/main/java/fragments/ComposeMsgFragment.java
@@ -7,9 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.*;
 import android.widget.AutoCompleteTextView;
-import android.widget.MultiAutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.cpsc41400.a4140app.R;
