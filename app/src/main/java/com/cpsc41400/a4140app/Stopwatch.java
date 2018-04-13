643ad1006c72af24cd0c23e0acfac5e184414e30
CarsonSallis <csallis@g.clemson.edu>
2018-04-13 02:16:43 -0400

    Almost have stopwatch working.

---
+++ app/src/main/java/com/cpsc41400/a4140app/Stopwatch.java
@@ -0,0 +1,8 @@
+package com.cpsc41400.a4140app;
+
+/**
+ * Created by user on 4/13/18.
+ */
+
+public class Stopwatch {
+}
---
+++ app/src/main/java/fragments/StopwatchFragment.java
@@ -0,0 +1,8 @@
+package fragments;
+
+/**
+ * Created by user on 4/13/18.
+ */
+
+public class StopwatchFragment {
+}
